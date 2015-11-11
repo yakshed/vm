9b2e757d54916b69ff1d1633e9afacaec29105ca
Sebastian Schulze <github.com@bascht.com>
2015-11-11 10:45:36 +0100

    [pk] Install roles during build

--- packer/scripts/ansible.sh
+++ packer/scripts/ansible.sh
@@ -3,3 +3,4 @@ apt-get install -y software-properties-common
 apt-add-repository -y ppa:ansible/ansible
 apt-get update
 apt-get install -y ansible
+ansible-galaxy install -r requirements.yml  -p roles/

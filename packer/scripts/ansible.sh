032fcbe92704bb2e99fe8694e6a9cc98e17c9555
Sebastian Schulze <github.com@bascht.com>
2016-06-29 16:35:38 +0200

    Bump to 0.2.x (including Ubuntu 16.04)

--- packer/scripts/ansible.sh
+++ packer/scripts/ansible.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-apt-get install -y software-properties-common
-apt-add-repository -y ppa:ansible/ansible
-apt-get update
 apt-get install -y ansible
 ansible-galaxy install -r /tmp/requirements.yml

ab4d19ab8b7923b23406b9e4ee3473a6235415a7
Sebastian Schulze <github.com@bascht.com>
2019-05-24 13:24:26 +0200

    [vagrant] Fix initial chown to use the uuid of myself

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -3,7 +3,7 @@
 echo vagrant:bascht | /usr/sbin/chpasswd
 find /home/vagrant
 mv /home/vagrant/.ssh /home/bascht/.ssh
-find /home/bascht -xdev -exec chown vagrant:vagrant {} +
+find /home/bascht -xdev ! -type l -exec chown 1000:1000 {} +
 sed -i s/vagrant/bascht/g /etc/passwd
 sed -i s/vagrant/bascht/g /etc/group
 sed -i s/vagrant/bascht/g /etc/shadow

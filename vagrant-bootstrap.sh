46c311ac22ad59dc6755f4079e69568a10b291b6
Sebastian Schulze <github.com@bascht.com>
2021-02-19 20:47:12 +0100

    [vagrant] Clean up bootstrap code

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 echo vagrant:bascht | /usr/sbin/chpasswd
-# find /home/vagrant
-
 mv /home/vagrant /home/bascht
-#mv /home/vagrant/.ssh /home/bascht/.ssh
 find /home/bascht -xdev ! -type l -exec chown 1000:1000 {} +
 sed -i s/vagrant/bascht/g /etc/passwd
 sed -i s/vagrant/bascht/g /etc/group
